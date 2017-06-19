8d83d97873f5935a7c16f8db4cbcd19fd47ba684
J Evans <g4@novadsp.com>
2017-06-19 12:08:39 +0100

    wrap exceptions

--- test.py
+++ test.py
@@ -5,19 +5,34 @@
 
 import numpy as np
 import example
+import sys, traceback
+
+# 
+np_i = np.array([0, 2, 4, 6], dtype=np.int)
+try:
+	avg = example.average_i(np_i)
+except Exception:
+	traceback.print_exc(file=sys.stdout)
+try:
+	avg = example.average_i(np_i.data,np_i.size)
+except Exception:
+	traceback.print_exc(file=sys.stdout)
+
 
 # fails
 np_i8 = np.array([0, 2, 4, 6], dtype=np.int8)
-avg = example.average_s8(np_i8.data,np_i8.size)
-print "int8 %d" % avg
+try:
+	avg = example.average_s8(np_i8)
+except Exception:
+	traceback.print_exc(file=sys.stdout)
+try:
+	avg = example.average_s8(np_i8.data,np_i8.size)
+except Exception:
+	traceback.print_exc(file=sys.stdout)
 
 # fails.
 np_u8 = np.array([0, 2, 4, 6], dtype=np.uint8)
 avg = example.average_u8(np_u8.data,np_u8.size)
 print "int8 %d" % avg
 
-# 
-np_i = np.array([0, 2, 4, 6], dtype=np.int)
-avg = example.average_i(np_i.data,np_i.size)
-print "int %d" % avg
 
